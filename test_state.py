b2cc685a8a89ab25761ec95a3d73971a5f7552dd
Tom Wood <thomas.a.wood@gmail.com>
2011-04-05 19:31:06 -0400

    Use unittest

--- test_state.py
+++ test_state.py
@@ -7,15 +7,17 @@ Created by Tom Wood on 2009-03-18.
 Copyright (c) 2009 __MyCompanyName__. All rights reserved.
 """
 
+import unittest
 from state import Machine
 
 class Request(object):
     m = Machine('pending')
     
     def notify_change(self, old, new):
-         print "%s: %s -> %s" % (self, old, new)
+        self.changes.append((old, new))
     
-    m.add_state(['assigned', 'started', 'completed', 'rejected', 'cancelled'], notify_change)
+    m.add_state(['assigned', 'started', 'completed', 'rejected', 'cancelled'],
+                notify_change)
     m.add_transition('pending', ['assigned', 'cancelled'])
     m.add_transition('assigned', 'started')
     m.add_transition('started', 'completed')
@@ -30,13 +32,25 @@ class Request(object):
     
     def __init__(self):
         self._state = Request.m.new_state(self)
-   
-def main():
-    req = Request()
-    req.state = 'assigned'
-    req.state = 'completed'
-    req.state = 'started'
-    
+        self.changes = []
+
+class TestStateMachine(unittest.TestCase):
+    def test_workflow(self):
+        req = Request()
+        req.state = 'assigned'
+        self.assertEquals(req.state, 'assigned')
+        # can't be completed
+        req.state = 'completed'
+        self.assertEquals(req.state, 'assigned')
+        req.state = 'started'
+        self.assertEquals(req.state, 'started')
+        req.state = 'completed'
+        self.assertEquals(req.state, 'completed')        
+        self.assertEquals(req.changes, [('pending', 'assigned'),
+                                        ('assigned', 'started'),
+                                        ('started', 'completed')])
+
 if __name__ == '__main__':
-	main()
+    unittest.main()
+
 
